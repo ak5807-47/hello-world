daebd7472e526c0fabf2e0b0b0772c6b9cba938f
ak5807-47 <user.email>
2026-03-11 10:17:58 +0530

    UC1: Display Hello World

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+/**
+ * HelloWorld
+ * Use Case 1 (UC1): Printing the literal text "Hello World" to the console.
+ * 
+ * @author ak5807-47
+ * @version 1.0
+ */
+public class HelloWorld {
+
+    /**
+     * The main method, serving as the application's entry point.
+     * 
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
